d46ac5498d5f105bdc3fd030ef21c0c5408cb78b
Kiarash <kiarashalvandpoor@gmail.com>
2020-01-25 19:54:56 +0330

    Upload games

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1579969089,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/card-sheet0.png",
+		"images/card-sheet1.png",
+		"images/stack-sheet0.png",
+		"images/suit-sheet0.png",
+		"images/sourse-sheet0.png",
+		"images/dealt-sheet0.png",
+		"images/background-sheet0.png",
+		"images/particle-sheet0.png",
+		"images/particle-sheet1.png",
+		"images/particle-sheet2.png",
+		"images/logo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
